60b24d0722009413ba6b2d6e2de2a7e2aac4eb4d
Wojciech Matusiak <wmatusiak@gmail.com>
2016-07-24 14:56:05 +0200

    Parser now skipping components without attributes

--- parser.go
+++ parser.go
@@ -11,19 +11,28 @@ import (
 func readAllComponents(in io.ReadCloser, out chan component, wait *sync.WaitGroup) {
 	scanner := bufio.NewScanner(bufio.NewReader(in))
 	var lines []string
+	var componenetFound bool
 	var inComponent bool
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line[0] == 'C' {
 			lines = make([]string, 0, 10)
-			inComponent = true
+			componenetFound = true
 			lines = append(lines, line)
-		} else if inComponent {
-			lines = append(lines, line)
-			if line[0] == '}' {
+		} else if componenetFound {
+			if line[0] == '{' {
+				inComponent = true
+			} else if !inComponent {
+				log.Printf("Found component without attributes. Skipping. (%s)", lines[0])
 				inComponent = false
+				componenetFound = false
+			} else if line[0] == '}' {
+				inComponent = false
+				componenetFound = false
 				comp := NewComponent(lines)
 				out <- comp
+			} else {
+				lines = append(lines, line)
 			}
 		}
 	}
